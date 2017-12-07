1a91a1d31726b3ebdbf3b8756915c0ee3ac409ac
逗叔 <yfb1004@163.com>
2017-12-07 23:21:59 +0800

    哈

---
+++ hi/hi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hi
+//
+//  Created by 逗叔 on 2017/12/7.
+//  Copyright © 2017年 逗叔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ hi/hi/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  hi
+//
+//  Created by 逗叔 on 2017/12/7.
+//  Copyright © 2017年 逗叔. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
